0452ff01fa473a436290c9c23fcef475d76f6da2
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-09-29 14:30:04 -0500

    class

--- script.js
+++ script.js
@@ -3,3 +3,76 @@ const SQUARES = 500;
 const WIDTH = 20;
 const HEIGHT = SQUARES / WIDTH;
 
+const container = document.getElementById("container");
+
+let activeIndex = 217;
+
+for (let i=0; i < SQUARES; i++) {
+    const square = document.createElement('div');
+    square.classList.add("square");
+
+    //on mouseover, add random color
+    square.addEventListener('mouseover', () => setColor(square))
+    //on mouseout, remove color
+    square.addEventListener('mouseout', () =>  removeColor(square))
+
+    container.appendChild(square);
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function setColor(element)
+{
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element){
+    element.style.background = "#1d1d1d";
+    element.style.boxShadow = `0 0 2px #1d1d1d, 0 0 10px #1d1d1d`;
+}
+
+function clear() {
+    const squares = doucment.getElementByClassName("square");
+
+    for (let square of squares) {
+        removeColor(square);
+    }
+}
+
+function blink () {
+    const squares = doucment.getElementByClassName("square");
+
+    for (let square of squares) {
+        setColor(square);
+        setTimeout( () => removeColor(square), 2000);
+    }
+}
+
+// function getSquareByIndex(index) {
+//     const square
+// }
+
+
+document.addEventListener('keypress', function( event ) {
+    if( event.code === "Space") {
+        //console.log("SPACE");
+        clear();    
+    }
+
+    if( event.code === "Enter") {
+        blink();    
+    }
+
+    if ( event.key == 'a') {
+        if (activeIndex % WIDTH === 0) {
+            activeIndex += WIDTH;
+        }
+        activeIndex = activeIndex -1;
+        setColor (getSquareByIndex(activeIndex));
+    }
+
+});
